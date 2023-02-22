5e634eac6277926e3011d2527456e620ee21eb2e
Divyaveer <86354945+Divyaveer@users.noreply.github.com>
2023-02-22 21:49:18 +0530

    Add files via upload

---
+++ text-animation.js
@@ -0,0 +1,19 @@
+const text = document.querySelector('.sec-text');
+
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = 'from Ahmedabad, Gujarat.';
+    }, 0);
+    setTimeout(() => {
+        text.textContent = 'a Full Stack Web Developer.';
+    }, 4000);
+    setTimeout(() => {
+        text.textContent = 'a Youtuber.';
+    }, 8000);
+    setTimeout(() => {
+        text.textContent = 'a Freelancer.';
+    }, 12000);
+};
+
+textLoad();
+setInterval(textLoad(), 16000);
